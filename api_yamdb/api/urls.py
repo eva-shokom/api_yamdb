91358e8f66e3932090051e577dcca94f5b2ae1b0
Евгения Кузьмина <eva.shokom@yandex.ru>
2023-12-29 18:05:12 +0300

    Создать модель review, сериалиатор, вьюсет, эндпойнт

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+from .views import ReviewViewSet, TitleViewSet
 
+
+app_name = 'api'
+
+v1_router = DefaultRouter()
+
+v1_router.register('titles', TitleViewSet, basename='title')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='review')
 
 urlpatterns = [
-    path("v1/", include(router.urls)),
+    path('v1/', include(v1_router.urls)),
 ]
