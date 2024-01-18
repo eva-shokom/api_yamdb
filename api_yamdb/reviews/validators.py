79df6a018cb85a6caa93a77f078bf11ffb364a30
Евгений Рычков <fakerichckov@yandex.ru>
2024-01-18 13:05:33 +0500

    Валидатор для поля year модели Title

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,9 +1,7 @@
-from django.core.validators import MaxValueValidator
+from django.core.exceptions import ValidationError
 from django.utils import timezone
 
 
-def validate_year():
-    MaxValueValidator(
-        timezone.now().year,
-        message='Значение года больше текущего!'
-    )
+def validate_year(value):
+    if not (0 < value <= timezone.now().year):
+        raise ValidationError('Проверьте год произведения!')
