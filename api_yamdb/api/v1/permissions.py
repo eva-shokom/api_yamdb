c551ddf1cae298ca5f363047051fe21da05b32dd
Orlan Uzum <uzumorlan@mail.ru>
2024-01-21 13:09:34 +0300

    fix7

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -4,10 +4,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsAdmin(BasePermission):
 
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and (request.user.is_admin or request.user.is_superuser)
-        )
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class IsAuthorOrAdminOrModeratorOrReadOnly(BasePermission):
@@ -18,7 +15,6 @@ class IsAuthorOrAdminOrModeratorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in SAFE_METHODS
-            or request.user.is_superuser
             or request.user.is_admin
             or request.user.is_moderator
             or obj.author == request.user
